4c14bb55661555b23e3f7bbe075869d61f644a84
M8Eggman <cagatay.morgul@outlook.com>
2025-09-12 14:23:44 +0200

    page connexion inscription fini

--- resources/js/Components/nav/Nav.jsx
+++ resources/js/Components/nav/Nav.jsx
@@ -1,60 +1,70 @@
-import { Link, usePage } from "@inertiajs/react";
+import { Link, router, usePage } from "@inertiajs/react";
 import { LuCar } from "react-icons/lu";
 import { GoGear, GoPlus } from "react-icons/go";
 
 export default function Nav() {
-  const { auth } = usePage().props;
+    const { auth } = usePage().props;
 
-  return (
-    <nav
-      className="flex flex-col items-center lg:flex-row justify-between gap-3 lg:gap-4 py-3 bg-white/50 backdrop-blur-sm shadow-md sticky top-0"
-      style={{ paddingInline: "max(8vw, 1rem)" }}
-    >
-      <Link href={route("home")} className="flex items-center gap-2.5 flex-shrink-0">
-        <LuCar className="text-3xl text-blue-900" />
-        <span className="text-xl font-medium bg-gradient-to-r from-blue-900 to-blue-700 bg-clip-text text-transparent">
-          AutoMarket
-        </span>
-      </Link>
-      <ul className="flex flex-col items-center lg:flex-row gap-2.5 lg:gap-5 flex-1 justify-center md:justify-start">
-        <li>
-          <Link className="font-medium hover:text-blue-700 transition-colors duration-300">Catalogue</Link>
-        </li>
-        <li>
-          <Link className="flex items-center gap-2 font-medium hover:text-blue-700 transition-colors duration-300">
-            <GoPlus className="inline mr-1" />
-            Vendez votre voiture
-          </Link>
-        </li>
-        <li>
-          <Link className="flex items-center gap-2 font-medium hover:text-blue-700 transition-colors duration-300">
-            <GoGear className="inline mr-1" />
-            Administration
-          </Link>
-        </li>
-      </ul>
-      <div className="flex gap-2.5 flex-wrap mt-2 md:mt-0">
-        {auth.user ? (
-          <button className="py-2 px-4 rounded-lg border border-gray-400 bg-white text-black hover:bg-blue-700 hover:text-white transition-all duration-300">
-            Se Déconnecter
-          </button>
-        ) : (
-          <>
+    return (
+        <nav
+            className="flex flex-col items-center lg:flex-row justify-between gap-3 lg:gap-4 py-3 bg-white/50 backdrop-blur-sm shadow-md sticky top-0"
+            style={{ paddingInline: "max(8vw, 1rem)" }}
+        >
             <Link
-              href={route("register")}
-              className="py-2 px-4 rounded-lg border border-gray-400 bg-white text-black transition-all duration-300 hover:text-white hover:bg-blue-700 hover:border-transparent"
+                href={route("home")}
+                className="flex items-center gap-2.5 flex-shrink-0"
             >
-              Connexion
+                <LuCar className="text-3xl text-blue-900" />
+                <span className="text-xl font-medium bg-gradient-to-r from-blue-900 to-blue-700 bg-clip-text text-transparent">
+                    AutoMarket
+                </span>
             </Link>
-            <Link
-              href={route("login")}
-              className="py-2 px-4 rounded-lg bg-blue-700 text-white transition-all duration-300 hover:bg-blue-900"
-            >
-              Inscription
-            </Link>
-          </>
-        )}
-      </div>
-    </nav>
-  );
+            <ul className="flex flex-col items-center lg:flex-row gap-2.5 lg:gap-5 flex-1 justify-center md:justify-start">
+                <li>
+                    <Link className="font-medium hover:text-blue-700 transition-colors duration-300">
+                        Catalogue
+                    </Link>
+                </li>
+                <li>
+                    <Link className="flex items-center gap-2 font-medium hover:text-blue-700 transition-colors duration-300">
+                        <GoPlus className="inline mr-1" />
+                        Vendez votre voiture
+                    </Link>
+                </li>
+                <li>
+                    <Link className="flex items-center gap-2 font-medium hover:text-blue-700 transition-colors duration-300">
+                        <GoGear className="inline mr-1" />
+                        Administration
+                    </Link>
+                </li>
+            </ul>
+            <div className="flex gap-2.5 flex-wrap mt-2 md:mt-0">
+                {auth.user ? (
+                    <div>
+                        <button
+                            onClick={() => router.post(route("logout"))}
+                            className="py-2 px-4 rounded-lg border border-gray-400 bg-white text-black hover:bg-blue-700 hover:text-white transition-all duration-300"
+                        >
+                            Se Déconnecter
+                        </button>
+                    </div>
+                ) : (
+                    <>
+                        <Link
+                            href={route("login")}
+                            className="py-2 px-4 rounded-lg border border-gray-400 bg-white text-black transition-all duration-300 hover:text-white hover:bg-blue-700 hover:border-transparent"
+                        >
+                            Connexion
+                        </Link>
+                        <Link
+                            href={route("register")}
+                            className="py-2 px-4 rounded-lg bg-blue-700 text-white transition-all duration-300 hover:bg-blue-900"
+                        >
+                            Inscription
+                        </Link>
+                    </>
+                )}
+            </div>
+        </nav>
+    );
 }
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,120 +1,139 @@
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import GuestLayout from "@/Layouts/GuestLayout";
+import { Head, Link, useForm } from "@inertiajs/react";
+import { LuCar } from "react-icons/lu";
 
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
     });
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('register'), {
-            onFinish: () => reset('password', 'password_confirmation'),
+        post(route("register"), {
+            onFinish: () => reset("password", "password_confirmation"),
         });
     };
 
     return (
-        <GuestLayout>
-            <Head title="Register" />
+        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+            <div>
+                <Link
+                    href={route("home")}
+                    className="flex items-center gap-2.5 flex-shrink-0"
+                >
+                    <LuCar className="text-5xl text-blue-900" />
+                    <span className="text-2xl font-medium bg-gradient-to-r from-blue-900 to-blue-700 bg-clip-text text-transparent">
+                        AutoMarket
+                    </span>
+                </Link>
+            </div>
+            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+                <Head title="Register" />
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="name" value="Name" />
+                <form onSubmit={submit}>
+                    <div>
+                        <InputLabel htmlFor="name" value="Name" />
 
-                    <TextInput
-                        id="name"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={(e) => setData('name', e.target.value)}
-                        required
-                    />
+                        <TextInput
+                            id="name"
+                            name="name"
+                            value={data.name}
+                            className="mt-1 block w-full"
+                            autoComplete="name"
+                            isFocused={true}
+                            onChange={(e) => setData("name", e.target.value)}
+                            required
+                        />
 
-                    <InputError message={errors.name} className="mt-2" />
-                </div>
+                        <InputError message={errors.name} className="mt-2" />
+                    </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
+                    <div className="mt-4">
+                        <InputLabel htmlFor="email" value="Email" />
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={(e) => setData('email', e.target.value)}
-                        required
-                    />
+                        <TextInput
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            className="mt-1 block w-full"
+                            autoComplete="username"
+                            onChange={(e) => setData("email", e.target.value)}
+                            required
+                        />
 
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
+                        <InputError message={errors.email} className="mt-2" />
+                    </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+                    <div className="mt-4">
+                        <InputLabel htmlFor="password" value="Password" />
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                        required
-                    />
+                        <TextInput
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            className="mt-1 block w-full"
+                            autoComplete="new-password"
+                            onChange={(e) =>
+                                setData("password", e.target.value)
+                            }
+                            required
+                        />
 
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
+                        <InputError
+                            message={errors.password}
+                            className="mt-2"
+                        />
+                    </div>
 
-                <div className="mt-4">
-                    <InputLabel
-                        htmlFor="password_confirmation"
-                        value="Confirm Password"
-                    />
+                    <div className="mt-4">
+                        <InputLabel
+                            htmlFor="password_confirmation"
+                            value="Confirm Password"
+                        />
 
-                    <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) =>
-                            setData('password_confirmation', e.target.value)
-                        }
-                        required
-                    />
+                        <TextInput
+                            id="password_confirmation"
+                            type="password"
+                            name="password_confirmation"
+                            value={data.password_confirmation}
+                            className="mt-1 block w-full"
+                            autoComplete="new-password"
+                            onChange={(e) =>
+                                setData("password_confirmation", e.target.value)
+                            }
+                            required
+                        />
 
-                    <InputError
-                        message={errors.password_confirmation}
-                        className="mt-2"
-                    />
-                </div>
+                        <InputError
+                            message={errors.password_confirmation}
+                            className="mt-2"
+                        />
+                    </div>
 
-                <div className="mt-4 flex items-center justify-end">
-                    <Link
-                        href={route('login')}
-                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                        Already registered?
-                    </Link>
+                    <div className="mt-4 flex items-center justify-end">
+                        <Link
+                            href={route("login")}
+                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        >
+                            Déjà inscris
+                        </Link>
 
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+                        <PrimaryButton className="ms-4" disabled={processing}>
+                            Inscription
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
