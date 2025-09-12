97d3a1b241938446a28204d4eae048e6db12898f
M8Eggman <cagatay.morgul@outlook.com>
2025-09-12 10:21:38 +0200

    model/migration user avatar role

---
+++ app/Models/Avatar.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Avatar extends Model
+{
+    /** @use HasFactory<\Database\Factories\AvatarFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'path',
+        'user_id'
+    ];
+}
