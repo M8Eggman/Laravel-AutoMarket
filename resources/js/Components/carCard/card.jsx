fa7cea1caa45eed3b5eddf71a99fdd8d48f5ab72
GoNEAR <cagatay.morgul@outlook.com>
2025-09-15 11:48:59 +0200

    creation carcard

--- resources/js/Components/carCard/card.jsx
+++ resources/js/Components/carCard/card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaCalendarAlt, FaRoad, FaGasPump, FaCogs } from "react-icons/fa";
 
 export default function Card({ car }) {
     return (
@@ -7,27 +8,35 @@ export default function Card({ car }) {
                 <img
                     src={`/storage/${car.image1_path}`}
                     alt={car.model}
-                    className="w-full h-48 object-cover"
+                    className="w-full h-60 object-cover"
                 />
-                <h2 className="font-semibold text-lg bg-gray-200 px-4 rounded-full z-10 absolute top-3 left-3">
-                    {car.brand?.name} {car.model}
-                </h2>
-            </div>
-
-            <div className="p-4 flex flex-col gap-2">
-                <span className="bg-gray-200 text-gray-800 text-xs px-2 py-1 rounded-full self-start">
+                <span className="bg-gray-200 text-gray-800 text-sm font-medium px-3 py-1 rounded-full absolute top-3 left-3">
                     {car.brand?.name}
                 </span>
-                <p className="text-blue-600 font-bold text-xl">
+            </div>
+            <div className="p-4 flex flex-col gap-2">
+                <h4 className="font-semibold text-h5">
+                    {car.brand?.name} {car.model}
+                </h4>
+                <p className="text-blue-800 font-bold text-h4">
                     {car.prix.toLocaleString()} €
                 </p>
-                <div className="flex flex-wrap gap-4 text-gray-600 text-sm">
-                    <span>📅 {car.annee}</span>
-                    <span>🛣 {car.kilometrage.toLocaleString()} km</span>
-                    <span>⛽ {car.fuel?.name}</span>
-                    <span>⚙ {car.type?.name}</span>
+                <div className="grid grid-cols-2 gap-4 text-gray-600 text-sm-custom">
+                    <span className="flex items-center gap-1">
+                        <FaCalendarAlt className="w-4 h-4" /> {car.annee}
+                    </span>
+                    <span className="flex items-center gap-1">
+                        <FaRoad className="w-4 h-4" />{" "}
+                        {car.kilometrage.toLocaleString()} km
+                    </span>
+                    <span className="flex items-center gap-1">
+                        <FaGasPump className="w-4 h-4" /> {car.fuel?.name}
+                    </span>
+                    <span className="flex items-center gap-1">
+                        <FaCogs className="w-4 h-4" /> {car.type?.name}
+                    </span>
                 </div>
-                <button className="mt-2 w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition">
+                <button className="mt-2 w-full bg-blue-700 text-white py-2.5 rounded hover:bg-blue-900 transition">
                     Voir détails
                 </button>
             </div>
